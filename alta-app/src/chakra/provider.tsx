8dbb5e387c67b5e14a470387d1430e3e8a556e9f
Mariano Desanze <protronm@gmail.com>
2025-08-09 19:30:38 -0300

    Arregle Toaster via "npx @chakra-ui/cli snippet add toaster"

--- alta-app/src/chakra/provider.tsx
+++ alta-app/src/chakra/provider.tsx
@@ -4,11 +4,13 @@ import { ChakraProvider } from '@chakra-ui/react'
 import { ColorModeProvider } from './color-mode'
 import { system } from './system'
 import type { ThemeProviderProps } from 'next-themes'
+import { Toaster } from './toaster'
 
 export function Provider(props: ThemeProviderProps) {
   return (
     <ChakraProvider value={system}>
       <ColorModeProvider {...props} />
+      <Toaster />
     </ChakraProvider>
   )
 }
