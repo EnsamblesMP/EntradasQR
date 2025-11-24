71d6fa69dd168af327418db4ee5a2071c73f3bbf
Mariano Desanze <protronm@gmail.com>
2025-11-24 17:14:22 -0300

    create-policies.sql faltaba RLS en cada tabla

--- docs/supabase-first-setup/create-policies.sql
+++ docs/supabase-first-setup/create-policies.sql
@@ -1,9 +1,16 @@
--- Allow access to all authenticated users
+-- All tables have RLS and allow access to all authenticated users
+
+alter table public.entradas
+enable row level security;
+
 create policy "Allow access to authenticated users"
   on entradas
   for all
   to authenticated
   using (true);
+
+alter table public.alumnos
+enable row level security;
 
 create policy "Allow access to authenticated users"
   on alumnos
@@ -11,17 +18,26 @@ create policy "Allow access to authenticated users"
   to authenticated
   using (true);
 
+alter table public.grupos
+enable row level security;
+
 create policy "Allow access to authenticated users"
   on grupos
   for all
   to authenticated
   using (true);
 
+alter table public.funciones
+enable row level security;
+
 create policy "Allow access to authenticated users"
   on funciones
   for all
   to authenticated
   using (true);
+
+alter table public.email_templates
+enable row level security;
 
 create policy "Allow access to authenticated users"
   on email_templates
