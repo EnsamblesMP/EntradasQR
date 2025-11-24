d35d58caeb249ce53039a39ec7d570e22d116029
Mariano Desanze <protronm@gmail.com>
2025-11-24 17:09:33 -0300

    Arreglé lint en AlumnoSelect

--- src/components/AlumnoSelect.tsx
+++ src/components/AlumnoSelect.tsx
@@ -24,7 +24,7 @@ const AlumnoSelect = ({
     data: alumnos,
     isLoading: loading,
     error: errorCarga,
-  } = useAlumnosPorGrupo(idGrupo ?? undefined);
+  } = useAlumnosPorGrupo(idGrupo);
   
   const alumnosCollection = createListCollection({
     items: alumnos || [],
